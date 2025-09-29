233b7602d289c9901a2f5fe11278ca09062bdac5
ivan.maestre.7e9 <ivan.maestre.7e9@itb.cat>
2025-09-29 13:21:44 +0200

    Arxius "HelloWorld" i "ComprovarEdat" acabats.

--- ComprovarEdat.py
+++ ComprovarEdat.py
@@ -1,1 +1,12 @@
 #Comprovar edat
+
+#Definir "Edat" com a número sencer + Demanar la edat + Insertar la edat
+edat=int(input("Quina edat tens?"))
+
+#Comprovant de si l'usuari és major o menor d'edat
+if edat>=18:
+    print("Ets major d'edat")
+    else
+    print("Ets menor d'edat")
+#Finalització del programa
+print("Programa Finalitzat")
